97eb4dd377a94af55ddb4bb6109485b0bb1f7d3b
Yordi <yordi.cruz@gmail.com>
2025-07-20 16:29:09 -0400

    users components

--- src/app/layout/component/app.menu.ts
+++ src/app/layout/component/app.menu.ts
@@ -33,8 +33,8 @@ export class AppMenu {
             {
                 label: 'Seguridad',
                 items: [
-                    { label: 'Usuarios', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/usuario'] },
-                    { label: 'Roles y Permisos', icon: 'pi pi-fw pi-check-square', routerLink: ['/admin/roles'] },
+                    { label: 'Usuarios', icon: 'pi pi-fw pi-id-card', routerLink: ['/admin/user'] },
+                    { label: 'Roles y Permisos', icon: 'pi pi-fw pi-check-square', routerLink: ['/admin/role'] },
 
                 ]
             },

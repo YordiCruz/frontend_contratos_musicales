3b4713aa674cc2b016862872a2c55b856efc72f9
Yordi <yordi.cruz@gmail.com>
2025-08-02 20:33:45 -0400

    modulo primeng

--- src/app/admin/admin-module.ts
+++ src/app/admin/admin-module.ts
@@ -5,32 +5,14 @@ import { AdminRoutingModule } from './admin-routing-module';
 import { Perfil } from './components/perfil/perfil';
 import { User } from './components/user/user';
 import { Role } from './components/role/role';
-import { DialogModule } from 'primeng/dialog';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import { TableModule } from 'primeng/table';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { CategoriaComponent } from './components/inventario/categoria-component/categoria-component';
 import { ProductoComponent } from './components/inventario/producto-component/producto-component';
 import { Inventario } from './components/inventario/inventario';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { ToolbarModule } from 'primeng/toolbar';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { SelectModule } from 'primeng/select';
 import { AlmacenComponent } from './components/inventario/almacen-component/almacen-component';
 import { SucursalComponent } from './components/inventario/sucursal-component/sucursal-component';
-
-import { TextareaModule } from 'primeng/textarea';
-
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { DatePickerModule } from 'primeng/datepicker';
-import { CheckboxModule } from 'primeng/checkbox';
-import { FileUploadModule } from 'primeng/fileupload';
-
+import { NotaCompraComponent } from './components/movimientos/nota-compra-component/nota-compra-component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {PrimengModule} from '../primeng/primeng-module'
 @NgModule({
   declarations: [
     Perfil,
@@ -41,30 +23,18 @@ import { FileUploadModule } from 'primeng/fileupload';
     Inventario,
     AlmacenComponent,
     SucursalComponent,
+    NotaCompraComponent,
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    DialogModule,
-    ButtonModule,
-    InputTextModule,
-    TableModule, 
     ReactiveFormsModule,
-    ProgressBarModule,
-    ProgressSpinnerModule,
-    BreadcrumbModule,
-    ToolbarModule,
-    IconFieldModule,
-    InputIconModule,
-    SelectModule,
-    TextareaModule,
-    RadioButtonModule,
-    InputNumberModule,
-    DatePickerModule,
-    CheckboxModule,
-    FileUploadModule,
+    
     //para usar ngmodel
-    FormsModule
-  ]
+    FormsModule,
+    PrimengModule
+  ],
+
+  
 })
 export class AdminModule { }
---
+++ src/app/admin/components/movimientos/nota-compra-component/nota-compra-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nota-compra-component',
+  standalone: false,
+  templateUrl: './nota-compra-component.html',
+  styleUrl: './nota-compra-component.scss'
+})
+export class NotaCompraComponent {
+
+}

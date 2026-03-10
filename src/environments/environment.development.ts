bf44ab6d1507cbe0304f7cbca890e3615c91ff32
Yordi <yordi.cruz@gmail.com>
2026-03-10 14:15:33 -0400

    configuracion de urls hacia el backend

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 export const environment = {
     //url_production: "http://127.0.0.1:8011/api/v1",
-    url_production: "http://127.0.0.1:3000",
+    //direccion del backend
+    url_production: "http://127.0.0.1:3070/admin",
+    
 
 
 };
